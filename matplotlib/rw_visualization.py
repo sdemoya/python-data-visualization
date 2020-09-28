616561a25e37cb9b68bee703d87e930a301f6fe3
skyedemoya <hello@skyedemoya.com>
2020-09-28 19:09:04 +0000

    Updated script.

--- matplotlib/rw_visualization.py
+++ matplotlib/rw_visualization.py
@@ -2,19 +2,35 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Create a random walk.
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    # Create a random walk.
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
 
-# Style plot/figure.
-plt.style.use('dark_background')
+    # Style plot/figure.
+    plt.style.use('dark_background')
 
-# Plot the walk's points.
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
+    # Plot the walk's points.
+    fig, ax = plt.subplots(figsize=(15, 9))
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.com.Blues,
+            edgecolors='none', s=15)
 
-# Open viewer.
-plt.show()
+    # Emphasize the first and last points.
+    ax.scatter(0,0, c='pink', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='purple', edgecolors='none',
+            s=100)
+
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # Open viewer.
+    plt.show()
+
+    keep_running = input("Plot another walk? (y/n):")
+    if keep_running == 'n':
+        break
 
 # Save plot/figure to a png.
 plt.savefig('random_walk.png', bbox_inches='tight')
