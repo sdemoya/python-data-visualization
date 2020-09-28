5958ce9aa5ae77f837fe5be5d8564aa7c68870f8
skyedemoya <hello@skyedemoya.com>
2020-09-28 16:19:22 +0000

    Updated cubes.py.

--- matplotlib/cubes.py
+++ matplotlib/cubes.py
@@ -1,26 +1,29 @@
 import matplotlib.pyplot as plt
 
-x_values = range(1, 5)
+x_values = range(1, 5000)
 y_values = [x**3 for x in x_values]
 
+# styles figure
+plt.style.use('fivethirtyeight')
 
-plt.style.use('seaborn')
-
+# function generates plots in the same figure
+fig, ax = plt.subplots()
 # fig represents the entire figure
 # ax represents a single plot within the figure
-fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=15)
-
+ax.scatter(x_values, y_values, c=y_values, cmap.plt.com.Blues, s=15)
 
 # Set titles
 ax.set_title('Cubed Numbers', fontsize=24)
 ax.set_xlabel('Value', fontsize=15)
 ax.set_ylabel('Cube of Value', fontsize=15)
-
+ax.tick_params(axis='both', which='major', labelsize=15)
 
 # Define range of axes
-ax.axis([0,5, 0, 125])
-
+ax.axis([0, 5000, 0, 125000000000])
 
 # Open figure in Matplotlib's viewer
 plt.show()
+
+# Automatically saves plot to a file and
+# trims extra whitespace from the figure.
+plt.savefig('cubes.png', bbox_inches'tight')
