abe1bcfecd209da7369332669874ad604c682e31
skyedemoya <hello@skyedemoya.com>
2020-11-11 00:27:14 +0000

    Added visualization.

--- plotly/die-visual.py
+++ plotly/die-visual.py
@@ -1,4 +1,7 @@
 from die import Die
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 
 #Create a D6.
 die = Die()
@@ -23,3 +26,14 @@ for value in range(1, die.num_sides+1):
     frequencies.append(frequency)
 
 print(frequencies)
+
+#Create a histogram.
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title = 'Results of Rolling on D6 1000 Times',
+    xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+
